ceb3008a28478eaf2a0607d3da35c0a833b20365
MKutluca <mert.kutluca@accenture.com>
2022-06-22 23:28:29 +0300

    init

---
+++ TO-DO_v2UITests/TO_DO_v2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TO_DO_v2UITestsLaunchTests.swift
+//  TO-DO_v2UITests
+//
+//  Created by mert.kutluca on 22.06.2022.
+//
+
+import XCTest
+
+class TO_DO_v2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
